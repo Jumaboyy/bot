67c746375493c962d7a87e731dadb4069d530977
Jumaboy <jumaboynematov280@gmail.com>
2024-03-02 18:51:46 +0500

    Bot yaratish

---
+++ vazif5.py
@@ -0,0 +1,56 @@
+# masala1
+# 1.	Books (kitoblar) haqida ma'lumotlarni o'zida saqlovchi o'zgarmas ro'yxat (tuple) xosjl qilinglar.
+# Shu tupleni ichidagi elementlarni murojaat qilishni osonlashtirish uchun named tuple dan foydalaninglar
+
+# from collections import namedtuple
+# Books=namedtuple('Book','book_name book_creator book_year')
+#
+# book1=('Python dasturlash asoslari','Anvar Narzullayev',2021)
+# book2=('Python o`rganamiz ','Matiz Erik',2021),
+# book3=(3,'Python o`rganamiz','Lutz Mark',2021),
+# book4=(4,'Python o`rganamiz','Mayer K',2021),
+#
+# kitob=Books(*book1)
+#
+# print(kitob.book_creator)
+# print(kitob.book_year)
+
+
+# masala2
+# 2.	Bot yozing. Bot ichida 3 ta comanda uchun 3 ta handler yozinglar. Botni guruhga ulanglar.
+# Bot Potga. yuborilgan matnli habarni guruhga yuborsin.
+# Bot bgtga yuborilgan rasmni guruhga yuborsin.
+# Bot botoa yuborilgan videoni guruhga yuborsin.
+# Bot botga yuborilgan animatsjvani guruhga yuborsin.
+# Botni GitHub ga yuklang. GitHubdagi bot linkini uyga vazifa faylini eng \J J	\J	A*VVVVVVVVVV J \J	J	\J
+# tepasiga Ctrl + V.
+
+from telebot import TeleBot
+
+bot=TeleBot('6895478934:AAGPYudmqSBbcNHmJcuyqz_7fMZ9UvlKvGk')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
